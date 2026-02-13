25242e36a5bcb499388489c7107de6f9f7a20495
ashfaqur-rashidmo <ashfaqurrashid1@gmail.com>
2026-02-13 20:50:52 +0600

    Fix CORS for deployed frontend

--- src/app.js
+++ src/app.js
@@ -7,9 +7,26 @@ import blogRoutes from "./routes/blogRoute.js";
 
 const app = express();
 
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   credentials: true,
+// }));
+
+const allowedOrigins = [
+  "http://localhost:3000", 
+  "https://shop-cart-frontend-eedd-oct0yxnah-ashfaqur-rashids-projects.vercel.app" 
+];
+
 app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
 
 app.use(express.json());
